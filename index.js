46697e3deb91e9d3ecfd4d523c621557055732e2
hello@bennyschmidt.com <hello@bennyschmidt.com>
2019-08-25 12:20:13 -0700

    add omit support

--- index.js
+++ index.js
@@ -19,15 +19,23 @@ exports.it = async (description, req, expect) => {
     delete res.data.timestamp;
   }
 
+  let received = res.data;
+
   console.log(`<< TEST >> ${description}`);
 
-  if (JSON.stringify(res.data) === JSON.stringify(expect)) {
-    console.log('\x1b[32m<< PASS >>\x1b[0m', res.data);
+  if (expect.hasOwnProperty('_omit')) {
+    expect._omit.forEach(k => k === delete received[k]);
+
+    delete expect._omit;
+  }
+
+  if (JSON.stringify(received) === JSON.stringify(expect)) {
+    console.log('\x1b[32m<< PASS >>\x1b[0m', received);
 
     return true;
   }
   else {
-    console.log(`\x1b[31m<< FAIL >>\x1b[33m\n\nEXPECTED: ${JSON.stringify(expect)}\n\x1b[31mRECEIVED: ${JSON.stringify(res.data)}\x1b[0m\n`);
+    console.log(`\x1b[31m<< FAIL >>\x1b[33m\n\nEXPECTED: ${JSON.stringify(expect)}\n\x1b[31mRECEIVED: ${JSON.stringify(received)}\x1b[0m\n`);
 
     return false;
   }
